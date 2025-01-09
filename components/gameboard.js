b5cc8ae675fba495259e763ff0d16b3189d6bb78
Sage Welky <ehrenwelky@gmail.com>
2025-01-09 16:16:17 -0600

    Created basic board logic to build off of

--- components/gameboard.js
+++ components/gameboard.js
@@ -1,7 +1,47 @@
-export default class GameBoard {
+//0 for nothing there
+//-1 for a miss already guessed, and -2 for a hit already guessed
+//1-10 for the corresponding unhit ship
+
+export default class Gameboard {
 
   constructor() {
 
+    this.spaces = [];
+    this.numShipsSunk = 0;
+    this.startBoard();
+
+    startBoard()
+    {
+      for(var i=0;i<=9;i++) {
+
+        this.spaces[i]=[];
+
+        for(var j=0;j<=9;j++) {
+
+          this.spaces[i].push(0);
+
+        }
+      }
+    }
+
+
+  }
+
+  receiveAttack(xCoord, yCoord) {
+
+    let check = this.space[xCoord][yCoord];
+
+    if(check === 0) {
+      this.space[xCoord][yCoord] = -1;
+      return ["Miss", check];
+    } else if(check < 0) {
+      return ["Already guessed", check];
+    } else if(check > 0 && check <11) {
+      this.space[xCoord][yCoord] = -2;
+      return ["Hit", check];
+    } else {
+      return ["Error", check];
+    }
   }
 
 
