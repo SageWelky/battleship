9a46264a1ff9968841f92c7ad1a9315de73869b7
Sage Welky <ehrenwelky@gmail.com>
2025-02-03 17:57:28 -0600

    Filled out ptmModal html and event listener logic.

--- src/dom/ptmModal.js
+++ src/dom/ptmModal.js
@@ -2,22 +2,45 @@ export function openPtmModal() {
 
   //display modal logic here:
   //NEEDS TO BE WRITTEN
+  const modalAnchor = document.getElementById('modal-anchor');
+  modalAnchor.innerHTML =
+  `
+  <div class="player-type-modal">
+    <div class="ptm-contaner" id="ptm-player-one-container">
+      <div class="ptm-option active" data-iscpu="true">Human</div>
+      <div class="ptm-option" data-iscpu="false">CPU</div>
+    </div>
+    <div class="ptm-contaner" id="ptm-player-two-container">
+      <<div class="ptm-option" data-iscpu="true">Human</div>
+      <div class="ptm-option active" data-iscpu="false">CPU</div>
+    </div>
+    <button id="ptm-confirm-button"></button>
+  </div>
+  `;
 
   //player selection logic here:
   return new Promise((resolve) => {
-    let playerTypeModal = document.querySelector( "#player-type-modal" );
     let ptmConfirmButton = document.querySelector( "#ptm-confirm-button" );
     let ptmPlayerOneContainer = document.querySelector( "#ptm-player-one-container" );
-    let ptmPlayerTwoContainer = document.querySelector( "#ptm-player-two-container" );\
+    let ptmPlayerTwoContainer = document.querySelector( "#ptm-player-two-container" );
 
-    playerTypeModal.style.display = "block";
+    function setPlayerType(event) {
+      event.target.closest("ptm-contaner").querySelectorAll(".ptm-option")
+      .forEach( option => option.classList.remove("active"));
+      event.target.closest("ptm-option").classList.add("active");
+    }
+
+    document.querySelectorAll(".ptm-option")
+    .forEach(option => option.addEventListener(click, setPlayerType(event)));
 
     function retreivePlayerTypes() {
       let playerOne = ptmPlayerOneContainer.dataset.iscpu === "true";
       let playerTwo = ptmPlayerTwoContainer.dataset.iscpu === "true";
 
+      document.querySelectorAll(".ptm-option")
+      .forEach(option => option.removeEventListener(click, setPlayerType(event)));
       ptmConfirmButton.removeEventListener("click", retreivePlayerTypes);
-      playerTypeModal.style.display = "none";
+      closePtmModal();
 
       resolve({ p1isCPU: playerOne, p2isCPU: playerTwo});
     }
@@ -27,6 +50,6 @@ export function openPtmModal() {
 }
 
 function closePtmModal() {
-  let playerTypeModal = document.querySelector("#player-type-modal");
-  playerTypeModal.style.display = "none";
+  const modalAnchor = document.getElementById('modal-anchor');
+  modalAnchor.replaceChildren();
 }
